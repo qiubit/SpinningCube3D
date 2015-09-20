28c1041d41ac7b47c2beced5eed1650f91df605e
Pawel Golinski <qiubit.bugzilla@gmail.com>
2015-09-20 21:13:54 +0200

    Delete unnecessary dependencies

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
+
+#include "cubewindow.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
     return app.exec();
 }
 
