ffc710b175b1829ceba5e5991dcf097a22237627
Pawel Golinski <qiubit.bugzilla@gmail.com>
2015-09-21 15:18:18 +0200

    Initialize OpenGL
    Draws a triangle in the window.

--- cubewindow.cpp
+++ cubewindow.cpp
@@ -1,7 +1,63 @@
+#include <QString>
+
 #include "cubewindow.h"
 
-CubeWindow::CubeWindow()
-{
+static GLfloat const triangleVertices[] = {
+    -0.75f, 0.75f, -1.0f, 1.0f,
+    -0.75f, -0.75f, -1.0f, 1.0f,
+    0.75f, -0.75f, -1.0f, 1.0f
+};
 
+static const char* vertexShaderSrc =
+        "#version 330\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+static const char* fragmentShaderSrc =
+        "#version 330\n"
+        "out vec4 outputColor;\n"
+        "void main()\n"
+        "{\n"
+        "   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+        "}\n";
+
+CubeWindow::CubeWindow(UpdateBehavior updateBehavior,
+                       QWindow *parent) :
+    QOpenGLWindow(updateBehavior, parent)
+{
 }
 
+void CubeWindow::resizeGL(int w, int h)
+{
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+}
+
+void CubeWindow::initializeGL()
+{
+    initializeOpenGLFunctions();
+    m_program = new QOpenGLShaderProgram(this);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSrc);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSrc);
+    m_program->link();
+    m_posAtr = m_program->attributeLocation("position");
+}
+
+void CubeWindow::paintGL()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    m_program->bind();
+
+    m_program->enableAttributeArray(m_posAtr);
+    m_program->setAttributeArray(m_posAtr, triangleVertices, 4);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    m_program->disableAttributeArray(m_posAtr);
+
+    m_program->release();
+}
--- cubewindow.h
+++ cubewindow.h
@@ -2,13 +2,28 @@
 #define CUBEWINDOW_H
 
 #include <QOpenGLWindow>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLFunctions>
 
-class CubeWindow : public QOpenGLWindow
+class CubeWindow : public QOpenGLWindow, protected QOpenGLFunctions
 {
     Q_OBJECT
 
 public:
-    CubeWindow();
+    explicit CubeWindow(UpdateBehavior updateBehavior = NoPartialUpdate,
+                        QWindow * parent = 0);
+    //~CubeWindow();
+
+protected:
+    virtual void initializeGL();
+    virtual void paintGL();
+    // virtual void paintOverGL();
+    // virtual void paintUnderGL();
+    virtual void resizeGL(int w, int h);
+
+private:
+    QOpenGLShaderProgram *m_program;
+    GLuint m_posAtr;
 
 signals:
 
